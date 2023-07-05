795e00e0c981abc167a39f5a9bfd95af42f5f0cd
jun7054 <135085171+jun7054@users.noreply.github.com>
2023-07-05 10:59:53 +0900

    Add files via upload

---
+++ 1511 이민준.py
@@ -0,0 +1,11 @@
+#스택 멤버 변수
+class IntStack{
+    int max;
+    int ptr; 
+    int[] stk;
+}
+#스택 매서드
+public int push(int x) 배가고파요{
+     if(ptr == max) 너무졸린데 시험 끝나서 너무 좋아요 ㅎㅎ();
+     return stk[ptr++] = x;
+ }
